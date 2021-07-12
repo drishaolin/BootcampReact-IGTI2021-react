4c98122273ebafe122fa812f63f6d27d62473bf7
Adriana Mucciolo <drishaolin@gmail.com>
2021-07-12 15:03:06 -0300

    reuse of Header and Main components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+
+export default function App() {
+  
+
+  return (
+    <div>
+      <Header>Projeto React Investments</Header>
+      
+      <Main>O conteúdo fica aqui</Main>
+      
+      
+    </div>
+  );
+}
