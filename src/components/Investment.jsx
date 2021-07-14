2bf480cfcb607945fe621449ac43a23e4d272502
Adriana Mucciolo <drishaolin@gmail.com>
2021-07-14 15:30:29 -0300

    color styles for positive and negative values

--- src/components/Investment.jsx
+++ src/components/Investment.jsx
@@ -5,6 +5,7 @@ export default function Investment({ children: invReport}) {
   const { month, year, value, percentage } = invReport;
   const monthsNames = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
   const currentMonth = monthsNames[month - 1];
+  const textColor = percentage >= 0 ? "text-green-600" : "text-red-600";
   
 
   
@@ -12,9 +13,10 @@ export default function Investment({ children: invReport}) {
   return (
     <div className="px-4">
       <ul>
-        <li className="flex flex-row border my-3 mx-10 ">
-          <span className="flex-1">{`${currentMonth}/${year} - R$ ${value.toFixed(2)}`}</span>
-          <PercentageFormat>{percentage}</PercentageFormat>
+        <li className="flex flex-row space-x-5 border my-3 mx-10 py-1 px-4 ">
+          <span>{`${currentMonth}/${year}`}</span>
+          <span className={`${textColor} flex-grow`}>{`R$ ${value.toFixed(2)}`}</span>
+          <PercentageFormat >{percentage}</PercentageFormat>
          
         </li>
       </ul>
--- src/components/PercentageFormat.jsx
+++ src/components/PercentageFormat.jsx
@@ -1,5 +1,5 @@
 export default function PercentageFormat({children: percent}) {
-  const percentColor = percent > 0 ? "text-green-600" : "text-red-600";
+  const percentColor = percent >= 0 ? "text-green-600" : "text-red-600";
   const sign = percent > 0 ? '+' : '';
   return <span className={percentColor}>{`${sign}${percent.toFixed(2)}%`}</span>;
 }
